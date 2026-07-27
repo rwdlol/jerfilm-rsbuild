990776fa4979a6a9ec42ae91499d3024aa753ef2
rawand-tech <info.rawandkhalid@gmail.com>
2026-07-27 14:47:36 +0200

    custom template

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,6 +4,9 @@ import { pluginTailwindcss } from '@rsbuild/plugin-tailwindcss';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  html: {
+    template: './static/index.html',
+  },
   plugins: [
     pluginReact({
       reactCompiler: true,
