f5a1af7c76a6b42bebe405645e54e5e1e5958b81
rawand-tech <info.rawandkhalid@gmail.com>
2026-07-27 13:43:44 +0200

    fix config and domain

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,16 +4,6 @@ import { pluginTailwindcss } from '@rsbuild/plugin-tailwindcss';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  output: {
-    distPath: {
-      root: 'dist', 
-    },
-    assetPrefix:
-      process.env.NODE_ENV === 'production'
-        ? '/jerfilm-rsbuild/'
-        : '/',
-  },
-
   plugins: [
     pluginReact({
       reactCompiler: true,
